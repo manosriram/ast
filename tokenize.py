2757fa6e3381a9610b58a09a2afe9e647b8a6b1f
Mano Sriram <mano.sriram0@gmail.com>
2025-01-12 02:20:52 +0530

    refactor tokenizer

--- tokenize.py
+++ tokenize.py
@@ -31,43 +31,43 @@ class Token:
 
 class Tokenizer:
     def __init__(self, source) -> None:
-        self._source = source
+        self.source = source
+        self.line = 0
+        self.position = 0
         self._tokens = []
-        self._line = 0
-        self._position = 0
 
     def eat(self, line=False):
-        self._position += 1
+        self.position += 1
         if line:
-            self._line += 1
+            self.line += 1
 
     def tokenize(self):
-        while self._position < len(self._source):
-            if self._source[self._position] in SKIP_TOKENS:
-                self.eat(line=self._source[self._position] == "\n")
-            elif self._source[self._position] in [TokenType.PLUS, TokenType.MULTIPLY, TokenType.DIVIDE, TokenType.MINUS, TokenType.LPAREN, TokenType.RPAREN]:
-                self._tokens.append(Token(TokenType(self._source[self._position]), self._source[self._position], line=self._line))
+        while self.position < len(self.source):
+            if self.source[self.position] in SKIP_TOKENS:
+                self.eat(line=self.source[self.position] == "\n")
+            elif self.source[self.position] in [TokenType.PLUS, TokenType.MULTIPLY, TokenType.DIVIDE, TokenType.MINUS, TokenType.LPAREN, TokenType.RPAREN]:
+                self._tokens.append(Token(TokenType(self.source[self.position]), self.source[self.position], line=self.line))
                 self.eat()
-            elif self._source[self._position].isdigit():
-                f = self._position
-                l = self._position
-                while self._position < len(self._source) and self._source[self._position].isdigit():
+            elif self.source[self.position].isdigit():
+                f = self.position
+                l = self.position
+                while self.position < len(self.source) and self.source[self.position].isdigit():
                     l += 1
                     self.eat()
-                self._tokens.append(Token(typ=TokenType.INTEGER, value=self._source[f:l], line=self._line))
-            elif self._source[self._position] == TokenType.SEMICOLON:
-                self._tokens.append(Token(typ=TokenType.SEMICOLON, value=TokenType.SEMICOLON.value, line=self._line))
+                self._tokens.append(Token(typ=TokenType.INTEGER, value=self.source[f:l], line=self.line))
+            elif self.source[self.position] == TokenType.SEMICOLON:
+                self._tokens.append(Token(typ=TokenType.SEMICOLON, value=TokenType.SEMICOLON.value, line=self.line))
                 self.eat()
-            elif self._source[self._position] == TokenType.ASSIGN:
-                self._tokens.append(Token(typ=TokenType.ASSIGN, value=TokenType.ASSIGN.value, line=self._line))
+            elif self.source[self.position] == TokenType.ASSIGN:
+                self._tokens.append(Token(typ=TokenType.ASSIGN, value=TokenType.ASSIGN.value, line=self.line))
                 self.eat()
-            elif self._position < len(self._source):
-                f = self._position
-                l = self._position
-                while self._position < len(self._source) and self._source[self._position] not in SKIP_TOKENS:
+            elif self.position < len(self.source):
+                f = self.position
+                l = self.position
+                while self.position < len(self.source) and self.source[self.position] not in SKIP_TOKENS:
                     l += 1
                     self.eat()
-                self._tokens.append(Token(typ=TokenType.VAR, value=self._source[f:l-1], line=self._line))
+                self._tokens.append(Token(typ=TokenType.VAR, value=self.source[f:l-1], line=self.line))
                 self.eat()
 
         return self
