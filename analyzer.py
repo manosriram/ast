5cf6c3191c5a12a775b8fee3ded2ed295ff0801d
Mano Sriram <mano.sriram0@gmail.com>
2025-01-12 21:08:37 +0530

    add Checker class

--- analyzer.py
+++ analyzer.py
@@ -4,7 +4,7 @@ source = """
 x = 123
 y = 234
 print(x+y)
-while 2 == 2:
+while 2 == 2 and 2 == 2:
     continue
 """
 
@@ -16,30 +16,34 @@ class While:
         self.node = node
         self.checks = []
 
-    def check_comparator(self):
-        if type(self.node.test.left) == ast.Constant and type(self.node.test.comparators[0]) == ast.Constant:
-            self.checks.append({ "check": CONDITION_ALWAYS_TRUE, "line": self.node.lineno })
+    def check_comparator(self, node):
+        if type(node.test.left) == ast.Constant and type(node.test.comparators[0]) == ast.Constant:
+            self.checks.append({ "check": CONDITION_ALWAYS_TRUE, "line": node.lineno })
             return False
 
-    def check_boolop(self):
+    def check_boolop(self, node):
         ok = True
-        #  for node in self.node.test.values:
-            #  ok &= self
-            #  ok &= self.check_comparator()
+        before = len(self.checks)
+        for node in node.test.values:
+            print("node ", node)
+            ok &= before != len(self.check(node).checks)
 
-    def check_constant(self):
-        self.checks.append({ "check": CONSTANT_WHILE_CONDITION, "line": self.node.lineno })
+        return ok
+     
+    def check_constant(self, node):
+        self.checks.append({ "check": CONSTANT_WHILE_CONDITION, "line": node.lineno })
         return False
 
-    def check(self):
-        if type(self.node.test) == ast.Constant:
-            self.check_constant()
-        if type(self.node.test) == ast.Compare:
-            self.check_comparator()
-        if type(self.node.test) == ast.BoolOp:
-            self.check_boolop()
+    def check(self, node):
+        print(node.__dict__)
+        if type(node.test) == ast.Constant:
+            self.check_constant(node)
+        elif type(node) == ast.Compare:
+            self.check_comparator(node)
+        elif type(node.test) == ast.BoolOp:
+            self.check_boolop(node)
 
-        return len(self.checks) == 0, self.checks
+        return self
 
 class Checker:
     def __init__(self, source) -> None:
@@ -50,9 +54,7 @@ class Checker:
     def check(self):
         for x in self.tree.body:
             if type(x) == ast.While:
-                ok, errors = While(x).check()
-                if not ok:
-                    self.checks.extend(errors)
+                self.checks.extend(While(x).check(x).checks)
 
         return self
                     
