e9678acec33900b5ded5df7fbca7de873427fc83
Mano Sriram <mano.sriram0@gmail.com>
2025-01-12 21:32:30 +0530

    analyzer: add checks for always true while condition

--- analyzer.py
+++ analyzer.py
@@ -4,6 +4,10 @@ source = """
 x = 123
 y = 234
 print(x+y)
+
+while True:
+    pass
+
 while 2 == 2 and 2 == 2:
     continue
 """
@@ -17,30 +21,29 @@ class While:
         self.checks = []
 
     def check_comparator(self, node):
-        if type(node.test.left) == ast.Constant and type(node.test.comparators[0]) == ast.Constant:
-            self.checks.append({ "check": CONDITION_ALWAYS_TRUE, "line": node.lineno })
+        if type(node.left) == ast.Constant and type(node.comparators[0]) == ast.Constant:
+            self.checks.append({ "check": CONDITION_ALWAYS_TRUE, "row": node.lineno, "col": node.end_col_offset })
             return False
 
     def check_boolop(self, node):
         ok = True
         before = len(self.checks)
         for node in node.test.values:
-            print("node ", node)
-            ok &= before != len(self.check(node).checks)
+            self.check(node)
+            ok &= before != len(self.checks)
 
         return ok
      
     def check_constant(self, node):
-        self.checks.append({ "check": CONSTANT_WHILE_CONDITION, "line": node.lineno })
+        self.checks.append({ "check": CONSTANT_WHILE_CONDITION, "row": node.lineno, "col": node.end_col_offset })
         return False
 
     def check(self, node):
-        print(node.__dict__)
-        if type(node.test) == ast.Constant:
+        if hasattr(node, "test") and type(node.test) == ast.Constant:
             self.check_constant(node)
         elif type(node) == ast.Compare:
             self.check_comparator(node)
-        elif type(node.test) == ast.BoolOp:
+        elif hasattr(node, "test") and type(node.test) == ast.BoolOp:
             self.check_boolop(node)
 
         return self
